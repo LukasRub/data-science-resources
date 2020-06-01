bcb30eec43ab8a17e2b2eb9cc75551cd1410bc37
Lukas Rubikas <lukas.rubikas@gmail.com>
2020-06-01 21:50:17 +0300

    TREC-IS 2020-A label preprocessing pipeline

--- data.scraping/twitter/TREC-IS.2020-A.example/src/preparation/clean_labels.py
+++ data.scraping/twitter/TREC-IS.2020-A.example/src/preparation/clean_labels.py
@@ -12,6 +12,7 @@ def simplify_event_ids(labels: pd.DataFrame, pattern=r'\w+\d{4}') -> pd.DataFram
     """Remove segmented data set indicators from eventID column
     """
     # Save orginal event ids (with segment indicators)
+    labels = labels.copy()
     labels['datasetID'] = labels.eventID 
 
     event_ids = labels.eventID.apply(lambda x: re.match(pattern, x).group())
@@ -54,14 +55,18 @@ def encode_post_priority(labels: pd.DataFrame, mapping='default') -> pd.DataFram
     labels = labels.drop(labels.index[not_in_mapping], axis='index')
 
     numerical_priority = labels.postPriority.map(mapping)
-    labels.postPriority = numerical_priority
+    labels['Priority'] = numerical_priority
+    labels = labels.drop(columns=['postPriority']) # Clean-up
 
     return labels
 
 
 def encode_post_categories(labels: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
+    # Get rid of annotations without assigned categories
+    labels = labels[labels.postCategories.apply(len) > 0] 
+
     _ = labels.postCategories.apply(pd.Series).stack()
-    one_hot_categories = pd.get_dummies(_).sum(level=0)
+    one_hot_categories = pd.get_dummies(_).sum(level=0).astype(np.int8)
 
     labels = labels.join(one_hot_categories)    
     labels = labels.drop(columns=['postCategories']) # Clean-up
@@ -73,7 +78,9 @@ def download_tweets(labels: pd.DataFrame, api_keys_path: str) -> pd.DataFrame():
     tweet_viewer = TweetViewer(api_keys_path=api_keys_path)
     tweets = []
     for labels_chunk in make_chunks(labels):
-        status_objs = tweet_viewer.pull_tweets(list(labels_chunk.postID))
+        status_objs = tweet_viewer.pull_tweets(
+            tweet_ids=list(labels_chunk.postID.astype(str)),
+            tweet_mode='extended', map_=True)
         tweets += [s._json for s in status_objs]
         print(f'Got {len(tweets)} tweets...')
     tweets = pd.DataFrame(tweets)
@@ -84,9 +91,14 @@ def download_tweets(labels: pd.DataFrame, api_keys_path: str) -> pd.DataFrame():
 if __name__  == '__main__':
     print('Importing and cleaning raw labels...')
     raw_labels_path = 'data/raw/train/labels/TRECIS_2018_2019-labels.json'
-    raw_labels = pd.read_json(raw_labels_path)
+    raw_labels = pd.read_json(raw_labels_path, orient='records',
+                              dtype=dict(postID=np.str_))
+    print(raw_labels.info())
+    print('Annotations before dropping duplicates...', len(raw_labels))
     raw_labels = raw_labels.drop_duplicates('postID', keep='last')
+    print('Annotations after dropping duplicates...', len(raw_labels))
     raw_labels = simplify_event_ids(raw_labels)
+    post_categories = raw_labels.copy()[['postID', 'postCategories']]
 
     print('Joining with topics file...')
     topics_path = 'data/raw/TRECIS-2018-2019.topics.xml'
@@ -101,12 +113,28 @@ if __name__  == '__main__':
     print('Downloading tweets...')
     api_keys_path = 'api_keys.yml'
     raw_tweets = download_tweets(labels, api_keys_path)
+    print(raw_tweets.info())
 
     # Drop no-longer-available tweets from both raw tweets and label dfs
-    print('Dropping no longer available tweets')
-    unavailable_tweets_idx = raw_tweets[raw_tweets.id_str.isna()].index
-    labels = labels.drop(unavailable_tweets_idx, axis='index')
-    raw_tweets = raw_tweets.drop(unavailable_tweets_idx, axis='index')
+    print('Dropping no longer available tweets...')
+    unavailable_tweets_ids = raw_tweets[raw_tweets.id_str.isna()]['id']
+    print(f'{len(unavailable_tweets_ids)} tweets are no longer available')
+    raw_tweets = raw_tweets[~raw_tweets.id.isin(unavailable_tweets_ids)]
+    labels = labels[~labels.postID.isin(unavailable_tweets_ids.astype(np.str_))]
+    print(f'{len(raw_tweets)} tweets available')
+    print(f'{len(labels)} annotations available')
+
+    # Drop non-english tweets
+    print('Dropping non-english tweets...')
+    lang_en = raw_tweets.lang == 'en'
+    print(f'{len(raw_tweets[~lang_en])} non-english tweets found')
+    raw_tweets = raw_tweets[lang_en]
+    labels = labels[labels.postID.isin(raw_tweets.id.astype(np.str_))]
+
+    # Check if no irrelevant tweets were downloaded
+    print('Dropping unannotated tweets...')
+    print(f'All tweets have annotations... {set(raw_tweets.id_str) == set(labels.postID)}')
+    raw_tweets = raw_tweets[raw_tweets.id_str.isin(labels.postID)]
 
     # Save processed label data
     print('Saving labels...')
@@ -116,10 +144,16 @@ if __name__  == '__main__':
                                          'TRECIS_2018_2019-labels.jsonl')
     cols = (['eventType', 'eventID', 'postID']
             + list(categories.columns)
-            + ['postPriority'])
+            + ['Priority'])
     processed_labels = labels[cols]
     processed_labels.to_json(processed_labels_path, 
                              orient='records', lines=True)
+
+    # Saving un-processed categories
+    print('Saving un-processed categories...')
+    post_categories_path = os.path.join(processed_labels_dir, 
+                                        'TRECIS_2018_2019-categories.jsonl')
+    post_categories.to_json(post_categories_path, orient='records', lines=True)
 
     # Save raw tweet data
     print('Saving tweets...')
