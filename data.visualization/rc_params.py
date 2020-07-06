1acbbf929ef28ad5a30f85ea1b1533c95b40c879
Lukas Rubikas <lukas.rubikas@gmail.com>
2020-07-06 20:10:32 +0300

    07/06 day progress

---
+++ data.visualization/rc_params.py
@@ -0,0 +1,13 @@
+rc = {
+    'axes.grid': False,
+    'axes.edgecolor': 'lightgrey',
+    'axes.spines.top': False,
+    'axes.spines.right': False,
+    'xtick.color': 'grey',
+    'xtick.labelsize': 'small',
+    'xtick.bottom': True,
+    'xtick.major.size': 2.5,
+    'ytick.color': 'grey',
+    'ytick.major.size': 2.5,
+    'ytick.left': True,
+}
